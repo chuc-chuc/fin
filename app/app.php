68c1405f669f3cf2968cc8e2b395f18a31b81086
Miguel Chuc <miguelchuc400@gmail.com>
2024-12-11 23:48:14 -0600

    nueva plantilla

---
+++ app/app.php
@@ -0,0 +1,13 @@
+<link href="css/datatables.min.css" rel="stylesheet">
+<script src="js/datatables.min.js"></script>
+
+<link href="css/select2.min.css" rel="stylesheet" />
+<script src="js/select2.min.js"></script>
+
+<link href="css/flowbite.min.css" rel="stylesheet" />
+<script src="js/flowbite.min.js"></script>
+
+<script src="js/tailwind.js"></script>
+
+<script src="js/sweetalert2.all.min.js"></script>
+<link href="css/sweetalert2.min.css" rel="stylesheet">
